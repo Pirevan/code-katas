17faed502f08050726e4cc014be35c91a5523330
Pirevan <38899303+Pirevan@users.noreply.github.com>
2018-05-22 13:23:06 +1000

    Add files via upload

---
+++ dayOne.rb
@@ -0,0 +1,34 @@
+
+
+
+array = "Fun !$ Time".split(/\W+/)
+
+
+
+
+
+
+z = ""
+i= 0
+while i < array.length do
+
+	if array[i].length > z.length
+		z = array[i]
+
+	end
+
+	i+=1
+
+end
+
+p z
+
+
+
+
+
+
+
+
+
+
