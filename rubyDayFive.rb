be426ebd9be7a6f96f083504f82c41ca50e5558a
Pirevan <38899303+Pirevan@users.noreply.github.com>
2018-05-30 12:24:29 +1000

    Add files via upload

---
+++ rubyDayFive.rb
@@ -0,0 +1,20 @@
+# First Way
+
+# def recur_fact(num)
+#   if num == 0 || num == 1
+#     1
+#   else
+#     num + recur_fact(num - 1)
+#   end
+# end
+
+# p recur_fact(4) # 720
+
+
+
+
+# def simpleAdd(num)
+# 	puts (1..num).inject(:+)
+# end
+
+
